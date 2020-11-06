2eb6b0063191065ec007207c374f72dd4f7d440e
AlexDon <1256625313@qq.com>
2020-11-06 17:39:10 +0800

    更新配置

--- .umi-production/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from 'C:/Users/weite/Downloads/landing-umi-example-master/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','request',],
-});
-
-export { plugin };
--- .umi-production/plugin-helmet/exports.ts
+++
@@ -1,3 +0,0 @@
-// @ts-nocheck
-// @ts-ignore
-export { Helmet } from 'C:/Users/weite/Downloads/landing-umi-example-master/node_modules/react-helmet';
